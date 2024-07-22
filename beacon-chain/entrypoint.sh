b5fc76e91af236c2b672d6bab9dc3bb20461dbc9
dappnodedev <diego@dappnode.io>
2024-07-22 10:44:18 +0200

    Use latest staker package scripts version

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -2,36 +2,35 @@
 
 SUPPORTED_NETWORKS="gnosis holesky mainnet"
 CHECKPOINT_SYNC_FLAG="--checkpoint-sync-url"
-MEVBOOST_FLAGS="--builder"
+MEVBOOST_FLAG_KEYS="--builder"
 
 # shellcheck disable=SC1091 # Path is relative to the Dockerfile
 . /etc/profile
 
-run_beacon() {
-    echo "[INFO - entrypoint] Starting beacon node..."
+ENGINE_URL=$(get_engine_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}")
+VALID_FEE_RECIPIENT=$(get_valid_fee_recipient "${FEE_RECIPIENT}")
+CHECKPOINT_SYNC_FLAG=$(get_checkpoint_sync_flag "${CHECKPOINT_SYNC_FLAG}" "${CHECKPOINT_SYNC_URL}")
+MEVBOOST_FLAG=$(get_mevboost_flag "${NETWORK}" "${MEVBOOST_FLAG_KEYS}")
 
-    # shellcheck disable=SC2086
-    exec lighthouse \
-        --debug-level "${LOG_LEVEL}" \
-        --network "${NETWORK}" \
-        beacon_node \
-        --datadir="${DATA_DIR}" \
-        --http \
-        --http-allow-origin "*" \
-        --http-address 0.0.0.0 \
-        --http-port "${BEACON_API_PORT}" \
-        --port "${P2P_PORT}" \
-        --metrics \
-        --metrics-address 0.0.0.0 \
-        --metrics-port 8008 \
-        --metrics-allow-origin "*" \
-        --execution-endpoint "${ENGINE_API_URL}" \
-        --execution-jwt "${JWT_FILE_PATH}" \
-        ${EXTRA_OPTS}
-}
+EXTRA_OPTS=$(add_flag_to_extra_opts_safely "${EXTRA_OPTS}" "--checkpoint-sync-url-timeout=300")
+EXTRA_OPTS=$(add_flag_to_extra_opts_safely "${EXTRA_OPTS}" "--suggested-fee-recipient=${VALID_FEE_RECIPIENT}")
 
-add_flag_to_extra_opts "--checkpoint-sync-url-timeout=300"
-set_beacon_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}"
-set_checkpointsync_url "${CHECKPOINT_SYNC_FLAG}" "${CHECKPOINT_SYNC_URL}"
-set_mevboost_flag "${MEVBOOST_FLAGS}" # MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
-run_beacon
+echo "[INFO - entrypoint] Starting beacon node..."
+
+# shellcheck disable=SC2086
+exec lighthouse \
+    --debug-level "${LOG_LEVEL}" \
+    --network "${NETWORK}" \
+    beacon_node \
+    --datadir="${DATA_DIR}" \
+    --http \
+    --http-allow-origin "*" \
+    --http-address 0.0.0.0 \
+    --http-port "${BEACON_API_PORT}" \
+    --port "${P2P_PORT}" \
+    --metrics \
+    --metrics-address 0.0.0.0 \
+    --metrics-port 8008 \
+    --metrics-allow-origin "*" \
+    --execution-endpoint "${ENGINE_URL}" \
+    --execution-jwt "${JWT_FILE_PATH}" ${CHECKPOINT_SYNC_FLAG} ${MEVBOOST_FLAG} ${EXTRA_OPTS}
--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -5,45 +5,39 @@ MEVBOOST_FLAG="--builder-proposals"
 SKIP_MEVBOOST_URL="true"
 CLIENT="lighthouse"
 
-handle_doppelganger() {
-    # If ENABLE_DOPPELGANGER != false, then enable it
-    if [ "${ENABLE_DOPPELGANGER}" != "false" ]; then
-        echo "[INFO - entrypoint] Doppelganger protection is enabled"
-        add_flag_to_extra_opts "--enable-doppelganger-protection"
-    else
-        echo "[WARN - entrypoint] Doppelganger protection is disabled"
-    fi
-}
-
 # shellcheck disable=SC1091
 . /etc/profile
 
-run_validator() {
-    echo "Starting validator..."
+VALID_GRAFFITI=$(get_valid_graffiti "${GRAFFITI}")
+VALID_FEE_RECIPIENT=$(get_valid_fee_recipient "${FEE_RECIPIENT}")
+BEACON_API_URL=$(get_beacon_api_url "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}")
+MEVBOOST_FLAG=$(get_mevboost_flag "${MEVBOOST_FLAG_KEY}" "${SKIP_MEVBOOST_URL}")
 
-    # shellcheck disable=SC2086
-    exec lighthouse \
-        --debug-level="${LOG_LEVEL}" \
-        --network="${NETWORK}" \
-        validator \
-        --init-slashing-protection \
-        --datadir "${DATA_DIR}" \
-        --beacon-nodes "${BEACON_API_URL}" \
-        --graffiti="${GRAFFITI}" \
-        --http \
-        --http-address 0.0.0.0 \
-        --http-port "${VALIDATOR_PORT}" \
-        --http-allow-origin "*" \
-        --unencrypted-http-transport \
-        --metrics \
-        --metrics-address 0.0.0.0 \
-        --metrics-port 8008 \
-        --metrics-allow-origin "*" \
-        --suggested-fee-recipient="${FEE_RECIPIENT}" ${EXTRA_OPTS}
-}
+if [ "${ENABLE_DOPPELGANGER}" != "false" ]; then
+    echo "[INFO - entrypoint] Doppelganger protection is enabled"
+    EXTRA_OPTS=$(add_flag_to_extra_opts_safely "${EXTRA_OPTS}" "--enable-doppelganger-protection")
+else
+    echo "[WARN - entrypoint] Doppelganger protection is disabled"
+fi
 
-handle_doppelganger
-format_graffiti
-set_validator_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}" "${CLIENT}"
-set_mevboost_flag "${MEVBOOST_FLAG}" "${SKIP_MEVBOOST_URL}" # MEV-Boost: https://chainsafe.github.io/lodestar/usage/mev-integration/
-run_validator
+echo "[INFO - entrypoint] Starting validator service"
+
+# shellcheck disable=SC2086
+exec lighthouse \
+    --debug-level="${LOG_LEVEL}" \
+    --network="${NETWORK}" \
+    validator \
+    --init-slashing-protection \
+    --datadir "${DATA_DIR}" \
+    --beacon-nodes "${BEACON_API_URL}" \
+    --graffiti="${VALID_GRAFFITI}" \
+    --http \
+    --http-address 0.0.0.0 \
+    --http-port "${VALIDATOR_PORT}" \
+    --http-allow-origin "*" \
+    --unencrypted-http-transport \
+    --metrics \
+    --metrics-address 0.0.0.0 \
+    --metrics-port 8008 \
+    --metrics-allow-origin "*" \
+    --suggested-fee-recipient="${VALID_FEE_RECIPIENT}" ${MEVBOOST_FLAG} ${EXTRA_OPTS}
