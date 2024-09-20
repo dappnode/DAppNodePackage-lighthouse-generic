b32a496a7c1131a65b3090f8a6e6e3709b28521b
dappnodedev <diego@dappnode.io>
2024-09-20 17:45:23 +0200

    Build flags separately

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -17,22 +17,24 @@ EXTRA_OPTS=$(add_flag_to_extra_opts_safely "${EXTRA_OPTS}" "--suggested-fee-reci
 JWT_SECRET=$(get_jwt_secret_by_network "${NETWORK}")
 echo "${JWT_SECRET}" >"${JWT_FILE_PATH}"
 
-echo "[INFO - entrypoint] Starting beacon node..."
+FLAGS="
+--debug-level=${LOG_LEVEL} \
+    --network=${NETWORK} \
+    beacon_node \
+    --datadir=${DATA_DIR} \
+    --http \
+    --http-allow-origin=* \
+    --http-address=0.0.0.0 \
+    --http-port=${BEACON_API_PORT} \
+    --port=${P2P_PORT} \
+    --metrics \
+    --metrics-address=0.0.0.0 \
+    --metrics-port=8008 \
+    --metrics-allow-origin=* \
+    --execution-endpoint=${ENGINE_URL} \
+    --execution-jwt=${JWT_FILE_PATH} $CHECKPOINT_SYNC_FLAG $MEVBOOST_FLAG $EXTRA_OPTS"
+
+echo "[INFO - entrypoint] Starting lighthouse beacon with flags: $FLAGS"
 
 # shellcheck disable=SC2086
-exec lighthouse \
-    --debug-level "${LOG_LEVEL}" \
-    --network "${NETWORK}" \
-    beacon_node \
-    --datadir="${DATA_DIR}" \
-    --http \
-    --http-allow-origin "*" \
-    --http-address 0.0.0.0 \
-    --http-port "${BEACON_API_PORT}" \
-    --port "${P2P_PORT}" \
-    --metrics \
-    --metrics-address 0.0.0.0 \
-    --metrics-port 8008 \
-    --metrics-allow-origin "*" \
-    --execution-endpoint "${ENGINE_URL}" \
-    --execution-jwt "${JWT_FILE_PATH}" ${CHECKPOINT_SYNC_FLAG} ${MEVBOOST_FLAG} ${EXTRA_OPTS}
+exec lighthouse $FLAGS
